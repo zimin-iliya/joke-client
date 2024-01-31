82f6609ed9371aa25763363c9471548ed5c8f667
zimin-iliya <zimin.box@gmail.com>
2024-02-01 00:39:48 +0200

    remove creds from login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,7 +17,6 @@ export default function Login() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
-        credentials: "include",
       });
       if (response.ok) {
         response.json().then((data) => {
